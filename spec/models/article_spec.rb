89f3ee55e76d41c6b233b80f299892c4e0c783d5
Walter Porzel <walter.porzel@thoughtworks.com>
2022-01-27 17:51:40 -0500

    added factory bot test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Article, type: :model do
   it "tests a number to be positive" do 
     expect(1).to be_positive #be_positive => 1.positive? == true
   end
+
+  it "tests article object" do
+    article = FactoryBot.create(:article)
+    expect(article.title).to eq("Sample article")
+  end
 end
